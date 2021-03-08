c388da80b6f0fb18fca1085c3195075c32b25d16
Mark Ruisdael <ruisdaeless@yahoo.com>
2021-03-08 22:43:18 +0400

    Try to complete

---
+++ Программирование - Халдузова/Практическая перед экзаменом/gringer/gringer/gringer/add_form.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace gringer
+{
+    public partial class add_form : Form
+    {
+        public add_form()
+        {
+            InitializeComponent();
+        }
+
+        public int Section { get; private set; }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            add_form dlg = new add_form();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                switch (dlg.Section)
+                {
+                    // case 1: addto_section1.Items.Add(dlg.input_filed); break;
+                    // case 2: addto_section2.Items.Add(dlg.input_filed); break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
